4fdc8d587ad1708921535ad528d480e220ffb617
qwer <58898485+khuiqel@users.noreply.github.com>
2026-08-17 19:31:58 -0700

    constexpr constants

--- tanks-game/_source/constants.h
+++ tanks-game/_source/constants.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstdint>
+#include <numbers>
 
 typedef int32_t Game_ID;
 typedef int8_t Team_ID;
@@ -16,25 +17,32 @@ enum class ObjectType : uint8_t {
 
 typedef int32_t Color_ID;
 
-extern const double PI;
-extern const double GAME_WIDTH;
-extern const double GAME_HEIGHT;
-extern const double TANK_RADIUS;
-extern const double SHOOT_COOLDOWN;
-extern const double POWER_COOLDOWN;
+constexpr double PI = std::numbers::pi;
+constexpr double GAME_WIDTH = 640;
+constexpr double GAME_HEIGHT = 320;
+constexpr double TANK_RADIUS = 16;
+constexpr double SHOOT_COOLDOWN = 100;
+constexpr double POWER_COOLDOWN = 500;
 
-extern const double BULLET_TO_TANK_RADIUS_RATIO;
-extern const float  BULLET_TO_TANK_SPEED_RATIO;
+constexpr double BULLET_TO_TANK_RADIUS_RATIO = .25;
+constexpr float  BULLET_TO_TANK_SPEED_RATIO = 4; //2 isn't that bad
 
-extern const float DESTRUCTION_TIER; //offense tier for destroying walls and turrets & stuff
-extern const float LOW_TIER;
-extern const float HIGH_TIER;
-extern const float LOW_IMPORTANCE;
-extern const float HIGH_IMPORTANCE;
+constexpr float DESTRUCTION_TIER = 2; //offense tier for destroying walls and turrets & stuff
+constexpr float LOW_TIER = -999;
+constexpr float HIGH_TIER = 999;
+constexpr float LOW_IMPORTANCE = -1;
+constexpr float HIGH_IMPORTANCE = 999;
 
-extern const Team_ID DEFAULT_TEAM; //walls and friends are this team
-extern const Team_ID HAZARD_TEAM;  //default team of hazards
-extern const Game_ID NO_PARENT;    //bullets that don't have a parent for parentID
+constexpr Team_ID DEFAULT_TEAM = 0; //walls and friends are this team
+constexpr Team_ID HAZARD_TEAM = -1; //default team of hazards
+constexpr Game_ID NO_PARENT = -1;   //bullets that don't have a parent for parentID
 
 //typedef void(*voidFunction)(void); //this isn't used
 //using voidFunction = void(*)(void); //C++11 version
+
+//tank team rules:
+//0  = no team or default team
+//-1 = hazard team (hazards can be split up into multiple teams, but by default there's only one)
+//1  = default teamID of tank #1 ("WASD")
+//2  = default teamID of tank #2 ("Arrow Keys")
+//tank teams always > 0
